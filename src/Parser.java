a7b7e8c3bce3a74e975888968f1544fafd5be91c
Sylcantor <guiblinnicolas@gmail.com>
2022-11-03 23:32:31 +0100

    update

--- src/Parser.java
+++ src/Parser.java
@@ -1,18 +1,18 @@
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+//import java.nio.file.Files;
+//import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Collections;
+import java.util.Scanner;
 import java.util.stream.IntStream;
-
 
 public class Parser {
     int numberOfVideos;
     int numberOfEndpoints;
     int numberOfRequests;
     int numberOfCacheServers;
+    int[] numberOfCacheServersUsed;
     int cacheServerCapacity;
     int k = 0;
 
@@ -22,84 +22,113 @@ public class Parser {
     CacheServer[] cacheServers;
     Request[] requests;
 
-    Parser(String file){
+    Parser(String file) {
         this.file = file;
     }
 
     public void setupData() throws IOException {
-        //a changer par System.in
-        byte[] encoded = Files.readAllBytes(Paths.get(file));
-        String fileContent = new String(encoded, StandardCharsets.UTF_8);
-        String[] lines = fileContent.split("\n");
-        String[] line = lines[k].split(" "); k++;
-        numberOfVideos = Integer.parseInt(line[0]);
-        numberOfEndpoints = Integer.parseInt(line[1]);
-        numberOfRequests = Integer.parseInt(line[2]);
-        numberOfCacheServers = Integer.parseInt(line[3]);
-        cacheServerCapacity = Integer.parseInt(line[4]);
+        // Scanner from system.in
 
-        line = lines[k].split(" "); k++;
+        Scanner sc = new Scanner(System.in, StandardCharsets.UTF_8);
+
+        /*
+         * byte[] encoded = Files.readAllBytes(Paths.get(file));
+         * String fileContent = new String(encoded, StandardCharsets.UTF_8);
+         * String[] lines = fileContent.split("\n");
+         * String[] line = lines[k].split(" ");
+         * k++;
+         */
+
+        numberOfVideos = sc.nextInt();
+        numberOfEndpoints = sc.nextInt();
+        numberOfRequests = sc.nextInt();
+        numberOfCacheServers = sc.nextInt();
+        numberOfCacheServersUsed = new int[numberOfCacheServers];
+        cacheServerCapacity = sc.nextInt();
+        // line = lines[k].split(" ");
+        // k++;
+
         videos = new Video[numberOfVideos];
-        for(int i=0;i<numberOfVideos;i++){
-            videos[i] = new Video(i,Integer.parseInt(line[i]));
+
+        for (int i = 0; i < numberOfVideos; i++) {
+            videos[i] = new Video(i, sc.nextInt());
         }
 
-
         endPoints = new EndPoint[numberOfEndpoints];
-        for(int i=0;i<numberOfEndpoints;i++){
-            line = lines[k].split(" "); k++;
-            int datacenterLatency = Integer.parseInt(line[0]);
-            int numberConnected = Integer.parseInt(line[1]);
-            endPoints[i] = new EndPoint(i,datacenterLatency,numberOfCacheServers);
+        for (int i = 0; i < numberOfEndpoints; i++) {
+            // line = lines[k].split(" ");
+            // k++;
+            int datacenterLatency = sc.nextInt();
+            int numberConnected = sc.nextInt();
+            endPoints[i] = new EndPoint(i, datacenterLatency, numberOfCacheServers);
 
-            for(int u=0;u<numberConnected;u++){
-                line = lines[k].split(" "); k++;
-                endPoints[i].cacheServers[Integer.parseInt(line[0])] = Integer.parseInt(line[1]);
+            for (int u = 0; u < numberConnected; u++) {
+                // line = lines[k].split(" ");
+                // k++;
+                endPoints[i].cacheServers[sc.nextInt()] = sc.nextInt();
             }
         }
 
-        requests = new  Request[numberOfRequests];
-        for(int i=0;i<numberOfRequests;i++){
-            line = lines[k].split(" "); k++;
-            requests[i] = new Request(i,Integer.parseInt(line[0]),Integer.parseInt(line[1]),Integer.parseInt(line[2]));
+        requests = new Request[numberOfRequests];
+        for (int i = 0; i < numberOfRequests; i++) {
+            // line = lines[k].split(" ");
+            // k++;
+            requests[i] = new Request(i, sc.nextInt(), sc.nextInt(), sc.nextInt());
         }
 
         cacheServers = new CacheServer[numberOfCacheServers];
-        for(int i=0;i<numberOfCacheServers;i++){
-            cacheServers[i] = new CacheServer(i,cacheServerCapacity,numberOfEndpoints,numberOfVideos);
+        for (int i = 0; i < numberOfCacheServers; i++) {
+            cacheServers[i] = new CacheServer(i, cacheServerCapacity, numberOfEndpoints, numberOfVideos);
         }
+        sc.close();
     }
 
-    void solver(){
-        Arrays.sort(requests);
-
-
+    void solver() {
+        Arrays.sort(requests, Collections.reverseOrder());
         int minCacheLatencyAvailable;
         int indexMinCacheLatency;
-        for (Request request: requests) {
-            int numberRequested = request.number;
-
+        for (Request request : requests) {
+            // int numberRequested = request.number;
             int finalVideoSize = videos[request.videoID].size;
             Integer[] finalEndPointCacheServerLatency = endPoints[request.endPointID].cacheServers;
             int finalEndPointDataServerLatency = endPoints[request.endPointID].dataCenterLatency;
-            if(IntStream.range(0,finalEndPointCacheServerLatency.length)
-                    .filter(i -> finalEndPointCacheServerLatency[i]>-1 && cacheServers[i].currentSpace>finalVideoSize
-                    && finalEndPointCacheServerLatency[i]< finalEndPointDataServerLatency)
-                    .mapToObj(i -> finalEndPointCacheServerLatency[i]).min(Integer::compare).isPresent()
-            ){
-                minCacheLatencyAvailable = IntStream.range(0,finalEndPointCacheServerLatency.length)
-                        .filter(i -> finalEndPointCacheServerLatency[i]>-1 && cacheServers[i].currentSpace>finalVideoSize)
+            if (IntStream.range(0, finalEndPointCacheServerLatency.length)
+                    .filter(i -> finalEndPointCacheServerLatency[i] > -1
+                            && cacheServers[i].currentSpace >= finalVideoSize
+                            && finalEndPointCacheServerLatency[i] < finalEndPointDataServerLatency)
+                    .mapToObj(i -> finalEndPointCacheServerLatency[i]).min(Integer::compare).isPresent()) {
+
+                minCacheLatencyAvailable = IntStream.range(0, finalEndPointCacheServerLatency.length)
+                        .filter(i -> finalEndPointCacheServerLatency[i] > -1
+                                && cacheServers[i].currentSpace >= finalVideoSize)
                         .mapToObj(i -> finalEndPointCacheServerLatency[i]).min(Integer::compare).get();
+
+                // System.out.println(minCacheLatencyAvailable);
+                indexMinCacheLatency = Arrays.asList(finalEndPointCacheServerLatency).indexOf(minCacheLatencyAvailable);
+                // System.out.println(indexMinCacheLatency);
+
+                cacheServers[indexMinCacheLatency].currentSpace -= finalVideoSize;
+                cacheServers[indexMinCacheLatency].videos[request.videoID] = 1;
+                numberOfCacheServersUsed[indexMinCacheLatency] = 1;
             }
-            else break;
-
-            //System.out.println(minCacheLatencyAvailable);
-            indexMinCacheLatency = Arrays.asList(finalEndPointCacheServerLatency).indexOf(minCacheLatencyAvailable);
-            //System.out.println(indexMinCacheLatency);
-
-            cacheServers[indexMinCacheLatency].currentSpace -= finalVideoSize;
         }
     }
 
-    void printResult(){}
+    void printResult() {
+        int i;
+        // sum of numberOfCacheServersUsed
+        int sum = Arrays.stream(numberOfCacheServersUsed).sum();
+        System.out.println(sum);
+        for (CacheServer cacheServer : cacheServers) {
+            if (cacheServer.currentSpace != cacheServerCapacity) {
+                System.out.print(cacheServer.cacheServerId);
+                for (i = 0; i < cacheServer.videos.length; i++) {
+                    if (cacheServer.videos[i] == 1) {
+                        System.out.print(" " + i);
+                    }
+                }
+                System.out.println();
+            }
+        }
+    }
 }
